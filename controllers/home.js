f22cd910d43fc6c24d550770d57c412a0052857b
Vicente Lucas <vica.escorcio@gmail.com>
2018-03-06 10:39:21 -0300

    connection with pdvmanager ok

--- controllers/home.js
+++ controllers/home.js
@@ -1,16 +1,23 @@
 module.exports = function(app) {
-  var restClient = require('../middlewares/restClient');
+  var restClient = require("../middlewares/restClient");
   var homeController = {
     index: function(req, res) {
       res.render("index");
     },
     test: function(req, res) {
-        restClient().methods.testMethod(function(data, response) {
-            // parsed response body as js object
-            console.log(data);
-            // raw response
-            console.log(response);
-          });;
+      args = {
+        headers: { "Content-Type": "application/json" },
+        data: {
+         "username": "admin",
+          "password": "123"
+        }
+      };
+      restClient().methods.testMethod(args,function(data, response) {
+        // parsed response body as js object
+        console.log(data);
+        // raw response
+        console.log(response.body);
+      });
       res.render("index");
     }
   };
--- middlewares/restClient.js
+++ middlewares/restClient.js
@@ -3,7 +3,8 @@ var Client = require('node-rest-client').Client;
 var client = new Client();
 
 module.exports = function(){
-    client.registerMethod("testMethod", "https://httpbin.org/ip", "GET");
+
+    client.registerMethod("testMethod", "http://201.38.172.82:8001/auth/login", "POST");
     return client;
 
 }
